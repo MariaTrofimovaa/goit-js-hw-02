db3beee39bc400f6283e8badde1b55be8a9c0715
MariaTrofimovaa <73877805+MariaTrofimovaa@users.noreply.github.com>
2021-05-05 10:14:37 +0300

    added link

--- js/task-6.js
+++ js/task-6.js
@@ -1,32 +1,25 @@
-'use strict'
+'use strict';
 
 let input;
 const numbers = [];
 let total = 0;
 
-
 do {
-    input = prompt('Введите число');
-    if (input) {
-        if (!isNaN(input)) {
-            numbers.push(Number(input));
-        } else {
-            console.log('Было введено не число, попробуйте еще раз');
-        }
+  input = prompt('Введите число');
+  if (input) {
+    if (!isNaN(input)) {
+      numbers.push(Number(input));
+    } else {
+      console.log('Было введено не число, попробуйте еще раз');
     }
+  }
 } while (input !== null);
-
 
 if (numbers.length) {
   for (const number of numbers) {
     total += number;
   }
-    console.log(`Общая сумма чисел равна ${total}`);
+  console.log(`Общая сумма чисел равна ${total}`);
 } else {
-  alert("Отмена пользователем");
+  alert('Отмена пользователем');
 }
-
-
-
-
-
